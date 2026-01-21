267d5ad4baa59bf4e179357e0ed032a901bf2c94
Arjun Sharma <arjunsharma6622@gmail.com>
2026-01-21 09:12:35 +0000

    initial commit

---
+++ infra/main.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "amazon_linux" {
+    most_recent = true
+
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+
+resource "aws_instance" "my_ec2" {
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = "t3.micro"
+
+    key_name = "poc10-linux-vm"
+
+    tags = {
+        Name = "poc10-linux-vm"
+    }
+
+}
